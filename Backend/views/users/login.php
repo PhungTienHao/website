1b7f590e34348ad0bfc281c2774e8980589ff106
phuc <nguyenngocphuc2002bv@gmail.com>
2023-08-21 15:53:15 +0700

    hao'

--- Backend/views/users/login.php
+++ Backend/views/users/login.php
@@ -1,57 +1,22 @@
 <?php
 //views/users/login.php
 ?>
-<body>
+<body class="body-user">
 <div class="container">
-    <h2> from đăng Nhập</h2>
+    <h2 class="h-user">  đăng Nhập</h2>
     <form action="" method="post" >
         <div class="form-group">
-            <label for="username">username</label>
-            <input type="text" id="username" name="username" class="form-control"> </div>
+
+            <input type="text" id="username" name="username" class="form-control" placeholder="tên đăng nhập "> </div>
         <div class="form-group">
-            <label for="password">password</label>
-            <input type="password" id="password" name="password" class="form-control"> </div>
+
+            <input type="password" id="password" name="password" class="form-control" placeholder="mật khẩu"> </div>
         <div class="form-group">
-            <input type="checkbox" >Ghi Nhớ Đăng Nhập <br>
+            <input type="checkbox" class="checkbox-user">Ghi Nhớ Đăng Nhập <br>
             <input type="submit" name="submit" value="đăng nhập" class="btn btn-success">
-            chưa có tài khoản , đăng ký tại <a href="index.php?controller=user&action=register">đây</a>
+            <div class="link-user">chưa có tài khoản , đăng ký  <a class="aaa" href="index.php?controller=user&action=register">tại đây</a>
+            </div>
         </div>
     </form>
 </div>
 </body>
-<style>
-
-    body{
-        background:url("assets/images/bk.jpg");
-
-    }
-    .container{
-        width: 30%;
-        margin: 100px 0 0 800px;
-        background: rgba(0 , 0 , 0 , 0.8);
-        box-shadow: 0 0 20px rgba(255 , 255 , 255 , 0.8);
-    }
-    .h-user{
-        color:#fff;
-        text-transform: uppercase;
-        text-align:center ;
-    }
-    .form-group{
-        border-bottom: 2px solid #fff;
-        color:#fff;
-        margin:5px;
-    }
-    .form-control{
-        background:transparent;
-        border: 0 ;
-        color:#fff
-    }
-    input.btn.btn-success{
-        width:100%;
-        margin-top:20px;
-    }
-    a{
-        font-size:30px;
-    }
-
-</style>
--- Backend/views/users/register.php
+++ Backend/views/users/register.php
@@ -5,32 +5,32 @@ require_once "models/User.php";
 ?>
 <body>
 <div class="container">
-    <h2 class="h-user"> from đăng ký</h2>
+    <h2 class="h-user">  đăng ký</h2>
     <form action="" method="post" enctype="multipart/form-data">
         <div class="form-group">
-            <label for="username">nhập tên đăng nhập</label>
-            <input type="text" id="username" name="username" class="form-control"> </div>
+
+            <input type="text" id="username" name="username" class="form-control" placeholder="tên đăng nhập"> </div>
         <div class="form-group">
-            <label for="password">nhập mật khẩu</label>
-            <input type="password" id="password" name="password" class="form-control"> </div>
+
+            <input type="password" id="password" name="password" class="form-control" placeholder="mật khẩu"> </div>
         <div class="form-group">
-            <label for="repas">nhập lại mật khẩu</label>
-            <input type="password" id="repas" name="repas" class="form-control"> </div>
+
+            <input type="password" id="repas" name="repas" class="form-control" placeholder="nhập lại mật khẩu"> </div>
         <div class="form-group">
-            <label for="name">nhập họ tên</label>
-            <input type="text" id="name" name="name" class="form-control">
+
+            <input type="text" id="name" name="name" class="form-control" placeholder="họ tên">
         </div>
         <div class="form-group">
-            <label for="phone" > nhập số điện thoại</label>
-            <input type="text" id="phone" name="phone" class="form-control">
+
+            <input type="text" id="phone" name="phone" class="form-control" placeholder="số điện thoại">
         </div>
         <div class="form-group">
-            <label for="email" > nhập email </label>
-            <input type="email" id="email" name="email" class="form-control">
+
+            <input type="email" id="email" name="email" class="form-control" placeholder="email">
         </div>
         <div class="form-group">
-            <label for="address" > nhập địa chỉ</label>
-            <textarea type="text" id="address" name="address" class="form-control" ></textarea>
+
+            <input type="text" id="address" name="address" class="form-control"placeholder="địa chỉ" >
         </div>
 
         <div class="form-group">
@@ -42,45 +42,7 @@ require_once "models/User.php";
             <input type="submit" name="submit" value="đăng ký"  class="btn btn-success">
 
         </div>
-        <div style="color:white; text-align: center">Đã có tài khoản , đăng nhập tại <a class="aaa" href="index.php?controller=user&action=login">đây</a></div>
+        <div class="link-user">Đã có tài khoản , đăng nhập  <a class="aaa" href="index.php?controller=user&action=login">tại đây</a></div>
     </form>
 </div>
 </body>
-<style>
-
-    body{
-        background:url("assets/images/bk.jpg");
-
-    }
-    .container{
-        width: 30%;
-        margin: 100px 0 0 800px;
-        background: rgba(0 , 0 , 0 , 0.8);
-        box-shadow: 0 0 20px rgba(255 , 255 , 255 , 0.8);
-    }
-    .h-user{
-        color:#fff;
-        text-transform: uppercase;
-        text-align:center ;
-    }
-    .form-group{
-        border-bottom: 2px solid #fff;
-        color:#fff;
-        margin:5px;
-    }
-    .form-control{
-        background:transparent;
-        border: 0 ;
-        color:#fff
-    }
-    input.btn.btn-success{
-        width:100%;
-        margin-top:20px;
-    }
-    a{
-        font-size:30px;
-    }
-
-</style>
-
-
