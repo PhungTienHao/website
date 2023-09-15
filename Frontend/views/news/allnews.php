8a94da9e5cb861ac6e0abe15276c0897d462e664
phuc <nguyenngocphuc2002bv@gmail.com>
2023-09-15 09:32:10 +0700

    hao

--- Frontend/views/news/allnews.php
+++ Frontend/views/news/allnews.php
@@ -13,43 +13,35 @@ require_once 'controllers/ProductController.php';
         </ol>
         <hr >
     </section>
-
+    <h2>Tin Tức Công Nghệ mới nhất</h2>
+    <?php if (!empty($news)): ?>
+    <div class="link-secondary-wrap ">
+        <?php foreach ($news AS $news):
+        $slug = Helper::getSlug($news['name']);
+        $product_link = "tin-tuc/$slug/" . $news['id'] . ".html";
+        ?>
     <div class="row" >
-        <div class="main-right col-md-9 col-sm-9 col-xs-12">
-            <h2>Tin Tức Công Nghệ mới nhất</h2>
-            <?php if (!empty($news)): ?>
-                <div class="link-secondary-wrap row">
-                    <?php foreach ($news AS $news):
-                        $slug = Helper::getSlug($news['name']);
-                        $product_link = "tin-tuc/$slug/" . $news['id'] . ".html";
-                        ?>
-                        <div class="service-link col-md-3 col-sm-6 col-xs-12">
+        <div class="main-right col">
+                <img class="secondary-img img-responsive" title="<?php echo $news['name'] ?>"
+                     src="../backend/assets/uploads/<?php echo $news['avatar'] ?>"     width="540px"  height="304px" >
+
+        </div>
+        <div class="service-link col ">
+            <b style="font-size:50px;"><?php echo $news['name'] ?></b>
                             <a href="<?php echo $product_link; ?>">
-                                <img class="secondary-img img-responsive" title="<?php echo $news['name'] ?>"
-                                     src="../backend/assets/uploads/<?php echo $news['avatar'] ?>"
-                                     alt="<?php echo $news['name'] ?>"/>
-                                <span class="shop-title">
+                                <span class="shop-title"></span>
                         <?php echo $news['summary'] ?>
-
-                        </div>
-                    <?php endforeach; ?>
-
-                </div>
-
+        </div>
+    </div>
+        <br>
+        <?php endforeach; ?>
             <?php endif;
 
             ?>
 
-        </div>
+
     </div>
 
 </div>
-<style>
-    .row{
-        display:grid;
-    }
-    .shop-title {
-        display: contents;
-    }
-</style>
+
 
